70c32cd043d40d8e8bf194c8ab31480349193628
Blayne Kettlewell <rbkettlewell@gmail.com>
2022-02-27 00:14:48 -0600

    added spi port new methods

--- src/spi.rs
+++ src/spi.rs
@@ -1,30 +1,98 @@
 use crate::gpio::p0::Parts;
-use crate::gpio::{Floating, Input, Output, Pin, PushPull};
+use crate::gpio::{AltFn, AltMode, Floating, Input, Level, Output, Pin, PushPull, AF1, AF2};
+use core::marker::PhantomData;
 use embedded_hal::spi::{FullDuplex, Mode, Phase, Polarity};
-use max32660_pac::SPI17Y as SPI0;
-use max32660_pac::SPIMSS as SPI1;
+// use max32660_pac::SPI17Y as SPI0;
+// use max32660_pac::SPIMSS as SPI1;
 use nb;
 
-pub enum SpiPort {
-    Spi0,
-    Spi1,
+pub struct Spi0;
+
+pub struct Spi1;
+
+pub struct Pins<
+    AF: AltMode,
+    const SCLK_IDX: u8,
+    const MISO_IDX: u8,
+    const MOSI_IDX: u8,
+    const SS_IDX: u8,
+> {
+    sclk: Pin<AF, Output<PushPull>, SCLK_IDX>,
+    miso: Pin<AF, Input<Floating>, MISO_IDX>,
+    mosi: Pin<AF, Output<PushPull>, MOSI_IDX>,
+    ss: Pin<AF, Output<PushPull>, SS_IDX>,
 }
 
-// pub struct Pins<AF>{
-//     //sclk: Pin<AF, Output<PushPull>>,
-//     miso: Option<Pin<AF, Input<Floating>>>,
-//     mosi: Option<Pin<AF, Output<PushPull>>>
-// }
+pub struct SpiPort<
+    AF: AltMode,
+    PORT,
+    const SCLK_IDX: u8,
+    const MISO_IDX: u8,
+    const MOSI_IDX: u8,
+    const SS_IDX: u8,
+> {
+    pins: Pins<AF, SCLK_IDX, MISO_IDX, MOSI_IDX, SS_IDX>,
+    _af: PhantomData<AF>,
+    _port: PhantomData<PORT>,
+}
 
-// pub fn configure_spi_pins(port: SpiPort, parts: Parts) -> Pins<AF1>{
-//     match port {
-//         SpiPort::Spi0 => {
-//             let mut miso = parts.p0_04.degrade();
-//             let miso1 = miso.into_floating_input();
-//             let miso2 = miso1.set_mode_af1();
-//             let pins = Pins{miso: Some(miso2), mosi: None };
-//             pins
-//         },
-//         _ => unimplemented!()
-//     }
-// }
+impl<AF: AltMode, PORT, const SCLK_IDX: u8, const MISO_IDX: u8, const MOSI_IDX: u8, const SS_IDX: u8,> 
+    SpiPort<AF, PORT, SCLK_IDX, MISO_IDX, MOSI_IDX, SS_IDX>
+{
+    fn configure_pins<SA: AltMode, IA: AltMode, OA: AltMode, XA: AltMode, SM, IM, OM, XM>(
+        sclk: Pin<SA, SM, SCLK_IDX>,
+        miso: Pin<IA, IM, MISO_IDX>,
+        mosi: Pin<OA, OM, MOSI_IDX>,
+        ss: Pin<XA, XM, SS_IDX>,
+    ) -> Pins<AF, SCLK_IDX, MISO_IDX, MOSI_IDX, SS_IDX>
+    where
+        Pin<AF, SM, SCLK_IDX>: AltFn,
+        Pin<AF, IM, MISO_IDX>: AltFn,
+        Pin<AF, OM, MOSI_IDX>: AltFn,
+        Pin<AF, XM, SS_IDX>: AltFn,
+    {
+        let sclk_mode = sclk.into_mode::<AF>();
+        let miso_mode = miso.into_mode::<AF>();
+        let mosi_mode = mosi.into_mode::<AF>();
+        let ss_mode = ss.into_mode::<AF>();
+        let sclk_electrical = sclk_mode.into_push_pull_output(Level::Low);
+        let miso_electrical = miso_mode.into_floating_input();
+        let mosi_electrical = mosi_mode.into_push_pull_output(Level::Low);
+        let ss_electrical = ss_mode.into_push_pull_output(Level::High);
+        Pins {
+            sclk: sclk_electrical,
+            miso: miso_electrical,
+            mosi: mosi_electrical,
+            ss: ss_electrical,
+        }
+    }
+}
+
+macro_rules! spi_ports{
+    (
+        [$(($new: ident, $AF: ident, $PORT: ident, $a: expr, $b: expr, $c: expr, $d: expr),)+]
+    ) => {
+        $(
+            impl SpiPort<$AF, $PORT, $a, $b, $c, $d>{
+                pub fn $new<SA:AltMode, IA: AltMode, OA: AltMode, XA: AltMode, SM, IM, OM, XM>(
+                    sclk: Pin::<SA, SM, $a>,
+                    miso: Pin::<IA, IM, $b>,
+                    mosi: Pin::<OA, OM, $c>,
+                    ss: Pin::<XA, XM, $d>
+                ) -> Self {
+                    let pins = Self::configure_pins(sclk, miso, mosi, ss);
+                    Self{
+                        pins: pins,
+                        _af: PhantomData,
+                        _port: PhantomData,
+                    }
+                }
+            }
+        )+
+    };
+}
+
+spi_ports!([
+    (new_spi_0, AF1, Spi0, 6, 4, 5, 7),
+    (new_spi_1, AF2, Spi1, 2, 0, 1, 3),
+]);
